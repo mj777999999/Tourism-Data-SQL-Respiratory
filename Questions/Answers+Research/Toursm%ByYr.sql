d95786c7548a85c4231bc32d49b60574373e7d44
mj777999999 <minjunchay2996@gmail.com>
2026-08-07 23:16:34 +0800

    Delete Questions/Answers+Research/Toursm%ByYr.sql

--- Questions/Answers+Research/Toursm%ByYr.sql
+++
@@ -1,7 +0,0 @@
-
-    SELECT 
-        destination_country,
-        year,
-        SUM(num_travelers) AS total_visitors
-    FROM global_tourism_travel_trends
-    GROUP BY destination_country, year;
